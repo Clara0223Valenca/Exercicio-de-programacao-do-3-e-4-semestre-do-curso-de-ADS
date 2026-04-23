34aceb4d7358a224e6d70300711ab4fec7695ef2
Clara Valença <clara.06840@aluno.iffar.edu.br>
2026-04-23 20:27:32 -0300

    feat: new exercise

---
+++ heranca/Pessoa.java
@@ -0,0 +1,16 @@
+/*1. Escreva uma classe chamada Pessoa com os atributos: nome, sexo, idade. Crie os métodos get/set para cada atributo e um construtor para a classe. Escreva agora outra classe chamada Amigo, que é uma pessoa (estende da classe Pessoa) de quem sabemos o dia de seu aniversário, com o atributo diaDoAniversario. Crie os métodos get/set para o atributo e um construtor para a classe (Lembre que esta classe herda os mesmos atributos de Pessoa). Defina as visibilidades adequadas para cada atributo/método nas duas classes.
+ */
+
+public class Pessoa {
+
+    private String nome;
+    private String sexo;
+    private int idade;
+
+    Pessoa (String nome,  String sexo, int idade) {
+        
+        this.nome = nome;
+        
+    }
+    
+}
