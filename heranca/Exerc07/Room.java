b0a1af1680f542ee0302b335ba009b5891ca2af0
Clara Valença <clara.06840@aluno.iffar.edu.br>
2026-05-18 20:37:29 -0300

    Create Room.java

---
+++ heranca/Exerc07/Room.java
@@ -0,0 +1,8 @@
+package Exerc07;
+/*7. Uma pousada deseja desenvolver um sistema para controlar suas suítes de luxo disponíveis para hospedagem. A superclasse deverá ser abstrata e se chamar Quarto, tendo como atributos privados: numeroQuarto, valorDiaria, ocupado (um boolean) e um método construtor. A subclasse deverá se chamar SuiteLuxo, com atributos privados: possuiHidromassagem (um boolean), quantidadeCamas, vistaMar (um boolean) e um construtor. Crie os métodos get e set para todos os atributos de ambas as classes. Na subclasse crie um método mostrarSuite() que exiba todas as informações da suíte, incluindo os atributos herdados da classe Quarto. No main, crie um objeto da classe SuiteLuxo e preencha todos os dados, depois exiba os dados da suíte chamando o método mostrarSuite(). Faça ainda, altere o status do quarto para ocupado, aumente o valor da diária para R$ 1150,00 e exiba novamente todas as informações atualizadas.
+18/05/2026*/
+
+public class Room {
+    
+    private int roomNumber;
+}
